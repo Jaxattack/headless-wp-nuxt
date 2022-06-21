ca070605de59345bf280d08740c7363b0fbe8942
Lance Jackson <lance.jackson01@gmail.com>
2022-06-21 15:09:47 +0200

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
       {
         rel: "stylesheet",
         href:
-          "https://fonts.googleapis.com/css2?family=Open+Sans"
+          "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400&display=swap"
       }
     ]
   },
@@ -114,7 +114,8 @@ export default {
   modules: [
     "@nuxtjs/sitemap",
     "@nuxtjs/robots",
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    // '@nuxt/image'
     ],
   /** Sitemap options
    * Doc: https://www.npmjs.com/package/@nuxtjs/sitemap
